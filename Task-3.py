0367aa0c8af451e9ddff12e6988b382396a3ea64
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-09-18 14:41:41 +0300

    Корректировка решения задачи 3

--- Task-3.py
+++ Task-3.py
@@ -1,30 +1,37 @@
 """ Создайте программу для игры в "Крестики-нолики". """
 
-
 import os
+
+
+def Game(lst, plyer, game_run):
+    Creating_field(lst)
+    while game_run:
+        print(f"Ходит игрок {plyer}")
+        try:
+            move_with = int(input("Позиция по горизонтали: "))  # Строка
+            move_height = int(input("Позиция по вертикали: "))  # Столбец
+            if move_with > len(lst) or move_height > len(lst):
+                print("Вы ввели большое число")
+            elif lst[move_height-1][move_with-1] == "---":
+                lst[move_height-1][move_with-1] = f"-{plyer}-"
+                Creating_field(lst)
+                game_run = Checking_winner(lst, plyer)
+                if plyer == "X":
+                    plyer = "O"
+                else:
+                    plyer = "X"
+            else:
+                print("Сделайте другой ход")
+        except:
+            print("Вы ввели не число")
+    else:
+        print("\nИгра окончена\n")
 
 
 def Creating_field(lst):
     os.system('cls')
     for item in lst:
         print(' '.join(map(str, item)))
-
-
-def Game(lst, plyer):
-    if plyer == "X":
-        plyer = "O"
-    else:
-        plyer = "X"
-    print(f"Ходит игрок {plyer}")
-    move_with = int(input("Позиция по горизонтали: "))  # Строка
-    move_height = int(input("Позиция по вертикали: "))  # Столбец
-    if lst[move_height-1][move_with-1] == "---":
-        lst[move_height-1][move_with-1] = f"-{plyer}-"
-    else:
-        print("Сделайте другой ход")
-        Game(lst, plyer)
-    Creating_field(lst)
-    Checking_winner(lst, plyer)
 
 
 def Checking_winner(lst, plyer):
@@ -47,17 +54,21 @@ def Checking_winner(lst, plyer):
     elif (lst[0][2] == "-X-" and lst[1][1] == "-X-" and lst[2][0] == "-X-") or (lst[0][2] == "-O-" and lst[1][1] == "-O-" and lst[2][0] == "-O-"):
         print(f"\nПобедитель {plyer}\n")
     else:
-        Game(lst, plyer)
+        for i in range(len(lst)):
+            for j in range(len(lst)):
+                if lst[j][i] == "---":
+                    return True
+        else:
+            return False
 
 
 field = [
-        ["---", "---", "---"],
-        ["---", "---", "---"],
-        ["---", "---", "---"],
+    ["---", "---", "---"],
+    ["---", "---", "---"],
+    ["---", "---", "---"],
 ]
 
-plyer = "O"
+plyer = "X"
+run = True
 
-Creating_field(field)
-
-Game(field, plyer)
+Game(field, plyer, run)
